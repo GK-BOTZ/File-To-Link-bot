6689493e80a27d2abf6fef7d7757264130dc012d
OFFLINE™ <134503777+nandan2023@users.noreply.github.com>
2024-09-15 13:57:32 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,20 +9,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '17108931'))
-    API_HASH = str(getenv('API_HASH', '436b24700208cae55ded351d8f25fd7a'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6998955142:AAHRLB4BfFMKf1x8SW-gd2IoS3GShkWhHTA'))
-    name = str(getenv('name', 'KRS_File_To_BoT'))
+    API_ID = int(getenv('API_ID', '11149186'))
+    API_HASH = str(getenv('API_HASH', 'd644038580c30a7d2d1fea67601d7427'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7364335027:AAEb70X-8T7epXKOAqV5F82mWJigihhZdcA'))
+    name = str(getenv('name', 'MR_X_FileToLink_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002113631575'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002380749200'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5300197778").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6094531111").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Kushi_Yarige_bedda'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'DMan282'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -32,12 +32,12 @@ class Var(object):
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.krsfiletolinkbot'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://krsfiletolinkbot-39fb9a40c0d9.herokuapp.com/".format(FQDN)
+        URL = "https://demmmmo-e076005fd584.herokuapp.com/".format(FQDN)
     else:
-        URL = "https://krsfiletolinkbot-39fb9a40c0d9.herokuapp.com/".format(FQDN)
+        URL = "https://demmmmo-e076005fd584.herokuapp.com/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://adminleech:UHee459K12WJBfDY@cluster0.xsmgkhi.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'KRS_BOTS'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002012506663")).split()))      
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002445426505")).split()))      
     SHORTLINK_URL = getenv('SHORTLINK_URL', 'onepagelink.in')
     SHORTLINK_API = getenv('SHORTLINK_API', 'c47e1c4469c0a66e74af7153cb8f4d3b304d010')
     TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/rk_back_up/18')
